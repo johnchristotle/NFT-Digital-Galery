4ffa2ea9442135950b26792086493f3078637477
agholor-christotle <agholorchristotle@gmail.com>
2022-11-20 21:29:34 +0100

    more nfts added

--- src/nfts.js
+++ src/nfts.js
@@ -1,16 +1,16 @@
-import Image1 from './assets/blink-guy.gif'; 
+import Image1 from 'https://media4.giphy.com/media/TJOKuSnGObvI72jVIu/200w.webp?cid=ecf05e47fd9ea7qynsk2d8vz05z238kxcckm45ldc9uoy991&rid=200w.webp&ct=g'; 
 import Image2 from './assets/blink-lady.gif';
-import Image3 from './assets/blinking-doll.gif';
-import Image4 from './assets/blinking-monkey.gif'; 
-import Image5 from './assets/chained-stars.gif';
-import Image6 from './assets/color-teeth.gif';
-import Image7 from './assets/glistering-box.gif'; 
+import Image3 from 'https://media1.giphy.com/media/88zFtljrXcHbhIhet8/200.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200.webp&ct=g';
+import Image4 from 'https://media3.giphy.com/media/1gbqIc1fK8QgR3bHL7/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
+import Image5 from 'https://media2.giphy.com/media/E44icdGPcAZDl0Vqeo/200w.webp?cid=ecf05e47fd9ea7qynsk2d8vz05z238kxcckm45ldc9uoy991&rid=200w.webp&ct=g';
+import Image6 from 'https://media4.giphy.com/media/w334hyb3HGGGGHWvRw/200w.webp?cid=ecf05e47r63zalyl1g9pyja28rpdukr0f5hcltmovtz0pxkc&rid=200w.webp&ct=g';
+import Image7 from 'https://media0.giphy.com/media/9Oacts8aQdNX3zEG3I/200w.webp?cid=ecf05e479psk9usvgbigtmx5cuvaaxehz6o1d7t5rdqy2rch&rid=200w.webp&ct=g'; 
 import Image8 from './assets/guy-smoking.gif';
-import Image9 from './assets/moving-etheruem.gif';
-import Image10 from './assets/red-eyes.gif'; 
+import Image9 from 'https://media0.giphy.com/media/99x4jSWHobFRPO7qWx/200w.webp?cid=ecf05e479psk9usvgbigtmx5cuvaaxehz6o1d7t5rdqy2rch&rid=200w.webp&ct=g';
+import Image10 from 'https://media3.giphy.com/media/H9M7lvORlmeFmvGoqY/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
 import Image11 from './assets/rotate-skeletons.gif';
-import Image12 from './assets/sad-monkey.gif';
-import Image13 from './assets/shiba-dog-head.gif'; 
+import Image12 from 'https://media0.giphy.com/media/Tb4Eunjfy4oSXGHSdY/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g';
+import Image13 from 'https://media1.giphy.com/media/HifsMXFLaRsmQlx8jo/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
 import Image14 from './assets/walking-duck.gif';
 
 
