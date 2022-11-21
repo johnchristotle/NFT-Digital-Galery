502fc99df27fbb0cf4f357c77e9bb06a8f9dd071
agholor-christotle <agholorchristotle@gmail.com>
2022-11-21 15:07:13 +0100

    more nfts added

--- src/nfts.js
+++ src/nfts.js
@@ -1,30 +1,22 @@
-import Image1 from 'https://media4.giphy.com/media/TJOKuSnGObvI72jVIu/200w.webp?cid=ecf05e47fd9ea7qynsk2d8vz05z238kxcckm45ldc9uoy991&rid=200w.webp&ct=g'; 
+//import Image1 from 'https://media4.giphy.com/media/TJOKuSnGObvI72jVIu/200w.webp?cid=ecf05e47fd9ea7qynsk2d8vz05z238kxcckm45ldc9uoy991&rid=200w.webp&ct=g'; 
 import Image2 from './assets/blink-lady.gif';
-import Image3 from 'https://media1.giphy.com/media/88zFtljrXcHbhIhet8/200.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200.webp&ct=g';
-import Image4 from 'https://media3.giphy.com/media/1gbqIc1fK8QgR3bHL7/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
-import Image5 from 'https://media2.giphy.com/media/E44icdGPcAZDl0Vqeo/200w.webp?cid=ecf05e47fd9ea7qynsk2d8vz05z238kxcckm45ldc9uoy991&rid=200w.webp&ct=g';
-import Image6 from 'https://media4.giphy.com/media/w334hyb3HGGGGHWvRw/200w.webp?cid=ecf05e47r63zalyl1g9pyja28rpdukr0f5hcltmovtz0pxkc&rid=200w.webp&ct=g';
-import Image7 from 'https://media0.giphy.com/media/9Oacts8aQdNX3zEG3I/200w.webp?cid=ecf05e479psk9usvgbigtmx5cuvaaxehz6o1d7t5rdqy2rch&rid=200w.webp&ct=g'; 
+//import Image3 from 'https://media1.giphy.com/media/88zFtljrXcHbhIhet8/200.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200.webp&ct=g';
+//import Image4 from 'https://media3.giphy.com/media/1gbqIc1fK8QgR3bHL7/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
+//import Image5 from 'https://media2.giphy.com/media/E44icdGPcAZDl0Vqeo/200w.webp?cid=ecf05e47fd9ea7qynsk2d8vz05z238kxcckm45ldc9uoy991&rid=200w.webp&ct=g';
+//import Image6 from 'https://media4.giphy.com/media/w334hyb3HGGGGHWvRw/200w.webp?cid=ecf05e47r63zalyl1g9pyja28rpdukr0f5hcltmovtz0pxkc&rid=200w.webp&ct=g';
+//import Image7 from 'https://media0.giphy.com/media/9Oacts8aQdNX3zEG3I/200w.webp?cid=ecf05e479psk9usvgbigtmx5cuvaaxehz6o1d7t5rdqy2rch&rid=200w.webp&ct=g'; 
 import Image8 from './assets/guy-smoking.gif';
-import Image9 from 'https://media0.giphy.com/media/99x4jSWHobFRPO7qWx/200w.webp?cid=ecf05e479psk9usvgbigtmx5cuvaaxehz6o1d7t5rdqy2rch&rid=200w.webp&ct=g';
-import Image10 from 'https://media3.giphy.com/media/H9M7lvORlmeFmvGoqY/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
+//import Image9 from 'https://media0.giphy.com/media/99x4jSWHobFRPO7qWx/200w.webp?cid=ecf05e479psk9usvgbigtmx5cuvaaxehz6o1d7t5rdqy2rch&rid=200w.webp&ct=g';
+//import Image10 from 'https://media3.giphy.com/media/H9M7lvORlmeFmvGoqY/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
 import Image11 from './assets/rotate-skeletons.gif';
-import Image12 from 'https://media0.giphy.com/media/Tb4Eunjfy4oSXGHSdY/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g';
-import Image13 from 'https://media1.giphy.com/media/HifsMXFLaRsmQlx8jo/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
+//import Image12 from 'https://media0.giphy.com/media/Tb4Eunjfy4oSXGHSdY/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g';
+//import Image13 from 'https://media1.giphy.com/media/HifsMXFLaRsmQlx8jo/200w.webp?cid=ecf05e47lbheqxgq5h17hyyesc5co6al6i54mcdvk78tt3h3&rid=200w.webp&ct=g'; 
 import Image14 from './assets/walking-duck.gif';
 
 
 export const nfts = [
 
-{
-    title: "Blinking Dude",
-    artist: "John Rose",
-    year: "2006",
-    movieID: "00001",
-    type: "NFT GIF",
-    image: Image1,
-    rating: 3,
-},
+
 {
     title: "Blinking Bitch",
     artist: "John Christotle",
@@ -34,51 +26,7 @@ export const nfts = [
     image: Image2,
     rating: 5,
 },
-{
-    title: "Blinking Doll",
-    artist: "John Daniel",
-    year: "2007",
-    movieID: "00003",
-    type: "NFT GIF",
-    image: Image3,
-    rating: 4,
-},
-{
-    title: "Blinking-Monkey",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00004",
-    type: "NFT GIF",
-    image: Image4,
-    rating: 5,
-},
-{
-    title: "Chained-Stars",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00005",
-    type: "NFT GIF",
-    image: Image5,
-    rating: 4,
-},
-{
-    title: "Color-Teeth",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00006",
-    type: "NFT GIF",
-    image: Image6,
-    rating: 3,
-},
-{
-    title: "Glistering-Box",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00007",
-    type: "NFT GIF",
-    image: Image7,
-    rating: 5,
-},
+
 {
     title: "Smoky-Bado",
     artist: "John Christotle",
@@ -88,24 +36,7 @@ export const nfts = [
     image: Image8,
     rating: 5,
 },
-{
-    title: "Moving-Etheruem",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00009",
-    type: "NFT GIF",
-    image: Image9,
-    rating: 4,
-},
-{
-    title: "Red Eyes",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00010",
-    type: "NFT GIF",
-    image: Image10,
-    rating: 5,
-},
+
 {
     title: "Rotate-Skeletons",
     artist: "John Christotle",
@@ -115,24 +46,7 @@ export const nfts = [
     image: Image11,
     rating: 4,
 },
-{
-    title: "Sad Monkey",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00012",
-    type: "NFT GIF",
-    image: Image12,
-    rating: 5,
-},
-{
-    title: "Shiba Dog",
-    artist: "John Christotle",
-    year: "2006",
-    movieID: "00013",
-    type: "NFT GIF",
-    image: Image13,
-    rating: 3,
-},
+
 {
     title: "Walking Duck",
     artist: "John Christotle",
